9bfa941b7d103fc7aeca740e469147d9570b4824
WinterCricket <hodhodhod2003@gmail.com>
2018-08-13 01:12:14 -0500

    add style, fix js link on index

--- callExternal.js
+++ callExternal.js
@@ -7,24 +7,23 @@
 
 function sniffer(message) {
 	console.log("You have clicked " + message);
-	document.getElementById('header').onclick=function(){sniffer('header')};
-	document.getElementById('pGraph').onclick=function(){sniffer('paragraphs')};
-	document.getElementById('list').onclick=function(){sniffer('list')};
-	document.getElementById('butt').onclick=function(){sniffer('button')};
+	
 
 };
 
+document.getElementById('header').onclick=function(){sniffer('header')};
+	document.getElementById('pGraph').onclick=function(){sniffer('paragraphs')};
+	document.getElementById('list').onclick=function(){sniffer('list')};
+	document.getElementById('butt').onclick=function(){sniffer('button')};
+// 
 
-// document.getElementsByTagName('input').onfocusin=function(){nameFunc(this)};
+function nameFunc(x) {
+ 	x.style.background = "yellow";}
 
-// function nameFunc(x) {
-// 	x.style.background = "yellow";}
-
-	function loader() {
-
+	document.getElementsByTagName('input').onfocusin=function(){nameFunc(this)};
 
 	
 	 document.getElementById('pageLoad').onload=function(){alert('Page Loading...')};
-	};
+	
 	
 	
