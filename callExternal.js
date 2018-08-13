ae721fbd17fdec16bf8fc0f0999f0ace9fba1784
WinterCricket <hodhodhod2003@gmail.com>
2018-08-13 12:45:27 -0500

    put variables and methods in funcs

--- callExternal.js
+++ callExternal.js
@@ -16,17 +16,31 @@ function sniffer(message) {
 };
 
 document.getElementById('header').onclick=function(){sniffer('header')};
-	document.getElementById('pGraph').onclick=function(){sniffer('paragraphs')};
-	document.getElementById('list').onclick=function(){sniffer('list')};
+document.getElementById('pGraph').onclick=function(){sniffer('paragraphs')};
+document.getElementById('list').onclick=function(){sniffer('list')};
 	// document.getElementsByTagName('button').onclick=function(){sniffer('button')};
 
-function nameFunc(x) {
- 	x.style.background = "yellow";}
+	function nameFunc(x) {
+		x.style.background = "yellow";}
 
-	document.getElementsByTagName('input').onfocusin=function(){nameFunc(this)};
+		document.getElementsByTagName('input').onfocusin=function(){nameFunc(this)};
 
-	
-	 document.getElementById('pageLoad').onload=function(){alert('Page Loading...')};
-	
-	
- document.getElementById('activator').onclick=function(){alert('This is what you ordered: ')};
+
+		document.getElementById('pageLoad').onload=function(){alert('Page Loading...')};
+
+
+		document.getElementById('activator').onclick=function(){alert('This is what you ordered: ')};
+
+		function numCheck() {
+		var myNum = "Holiday Harry";
+		var result = isNaN(myNum);
+		console.log(result);
+}
+
+		function intCheck() {
+		var myNum = "42 natural gas units";
+		var result = parseInt(myNum);
+		console.log(result);
+		console.log(result + 42);
+		console.log(myNum + 42);
+}
